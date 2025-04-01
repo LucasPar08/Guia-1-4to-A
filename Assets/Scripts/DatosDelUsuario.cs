fa1cf1cdf5d8a7bb4e8b0541e9c9789959164e55
LucasPar08 <lucaspark2008@gmail.com>
2025-04-01 11:39:23 -0300

    Ejercicio 2 terminado

--- Assets/Scripts/DatosDelUsuario.cs
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -12,7 +12,8 @@ public class DatosDelUsuario : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Mi nombre es " + nombreUsuario);
+        Debug.Log("Mi nombre es " + nombreUsuario   + ", tengo " + edadUsuario + ",mido" + 
+            alturaUsuario + "metros y es" + esDonante + "que soy donante de organos.");
         Debug.Log("Tengo " + edadUsuario + " años.");
     }
 
